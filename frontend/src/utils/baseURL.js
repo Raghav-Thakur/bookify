e58546f914772b84aba6d0f00e5c2fcffe8c9001
Raghav Thakur <you@example.com>
2024-12-09 14:52:00 +0530

    update

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,5 +1,6 @@
 const getBaseUrl = () => {
-    return "http://localhost:5000"
+    return   "https://bookify-2wo28wzea-raghav-thakurs-projects-8d9686fe.vercel.app"
+    // return "http://localhost:5000"
 }
 
 export default getBaseUrl;
